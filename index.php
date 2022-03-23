11d17454c8374c023de78eb263ede02dd58b09a5
Khadiza <khadizaakhi756@gmail.com>
2022-03-23 21:52:55 +0600

    first commit

--- index.php
+++ index.php
@@ -12,6 +12,7 @@
 </head>
 
 <body>
+    <!-- Home Page -->
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="container-fluid">
             <a class="navbar-brand" href="#">Photo Gallery</a>
@@ -58,6 +59,54 @@
             </div>
         </div>
     </nav>
+
+    <!-- Photo Slider -->
+    <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-indicators">
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active"
+                aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1"
+                aria-label="Slide 2"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2"
+                aria-label="Slide 3"></button>
+        </div>
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="images/n1.jpg" class="d-block w-100" alt="nature">
+                <div class="carousel-caption d-none d-md-block">
+                    <h3>
+                        Nature
+                    </h3>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img src="images/a1.jpg" class="d-block w-100" alt="architecture">
+                <div class="carousel-caption d-none d-md-block">
+                    <h3>
+                        Architecture
+                    </h3>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img src="images/t1.jpg" class="d-block w-100" alt="travel">
+                <div class="carousel-caption d-none d-md-block">
+                    <h3>
+                        Travel
+                    </h3>
+                </div>
+            </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators"
+            data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators"
+            data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
+    </div>
 
 
 
